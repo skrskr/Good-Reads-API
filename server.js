1b9a653a8d15c320f5e0e8a5af3a96031dd1fc98
skrskr <mohamed1.sakr1996@gmail.com>
2019-12-29 16:31:21 +0200

    adding error middleware

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Handle Errors Middlewares
+// 404 Not found
+app.use((req, res, next) => {
+  const err = new Error("Resource Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// handle errors Middleware
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: err.message
+  });
+});
+
 app.listen(PORT, err => {
   if (!err) console.log(`Server running in ${PORT} port`);
 });
